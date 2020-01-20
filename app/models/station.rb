364508fb4a552be13cb7450eb824df5e5647171a
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-20 12:10:28 +0900

    migration file作成

---
+++ app/models/station.rb
@@ -0,0 +1,3 @@
+class Station < ApplicationRecord
+  belongs_to :propety
+end
---
+++ db/migrate/20200120030127_create_properties.rb
@@ -0,0 +1,12 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.text :address
+      t.integer :age
+      t.text :note
+
+      t.timestamps
+    end
+  end
+end
